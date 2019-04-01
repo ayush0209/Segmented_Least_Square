559651a62450b79318f2689873bb6dcce6eb67a9
ayush0209 <45902935+ayush0209@users.noreply.github.com>
2019-04-02 01:25:29 +0530

    Create PointsOps.cpp

---
+++ PointsOps.cpp
@@ -0,0 +1,47 @@
+#include<stdint.h>
+#include<vector.h>
+#include<cmath.h>
+#include"Point.h"
+#include"PointsOps.h"
+using namespace std;
+
+double PointsOps::getXsum(std::vector<Point> &points,int num_points)
+{
+	double sumX=0;
+	for(int i=0;i<num_points;i++)
+	{
+		sumX+=points[i].getX();
+	}
+	return sumX
+}
+
+double PointsOps::getYsum(std::vector<Point> &points,int num_points)
+{
+	double sumY=0;
+	for(int i=0;i<num_points;i++)
+	{
+		sumY+=points[i].getY();
+	}
+	return sumY
+}
+
+double PointsOps::getXYsum(std::vector<Point> &points,int num_points)
+{
+	double sumXY=0;
+	for(int i=0;i<num_points;i++)
+	{
+		sumXY+=(points[i].getX()*points[i].getY());
+	}
+	return sumXY
+}
+
+
+double PointsOps::getXsquaredsum(std::vector<Point> &points,int num_points)
+{
+	double sumX2=0;
+	for(int i=0;i<num_points;i++)
+	{
+		sumX2+=pow(points[i].getX(),2);
+	}
+	return sumX2
+}
