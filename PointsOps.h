edc39890cdea2752b1b8cc2589a2102ce85923c2
ayush0209 <45902935+ayush0209@users.noreply.github.com>
2019-04-02 01:25:59 +0530

    Create PointsOps.h

---
+++ PointsOps.h
@@ -0,0 +1,16 @@
+#ifndef POINTSOPS_H
+#define POINTSOPS_H
+#include<stdint.h>
+#include<vector.h>
+#include"Point.h"
+
+class PointOps
+{
+	public:
+		double PointsOps::getXsum(std::vector<Point> &points,int num_points);
+		double PointsOps::getYsum(std::vector<Point> &points,int num_points);
+		double PointsOps::getXYsum(std::vector<Point> &points,int num_points);
+		double PointsOps::getXsquaredsum(std::vector<Point> &points,int num_points);
+};
+
+#endif
