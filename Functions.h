3c6963178c8318cf182594f6e045bf0a2e814626
ayush0209 <45902935+ayush0209@users.noreply.github.com>
2019-04-02 01:23:47 +0530

    Create Functions.h

---
+++ Functions.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include<stdint.h>
+#include<vector.h>
+#include"Point.h"
+
+class Functions
+{
+	public:
+		double getBeta(std::vector<Point> &points,int num_points);
+		double getAplha(std::vector<Point> &points,int num_points,double beta);
+		double leastsquareError(std::vector<Point> &points,int size);
+};
+
+#endif
