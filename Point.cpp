6b0d58168232836f18889e6f1e4410e7cdf0c373
ayush0209 <45902935+ayush0209@users.noreply.github.com>
2019-04-02 01:30:54 +0530

    defined functions in Point class

---
+++ Point.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "Point.h"
+Point::Point()
+{
+	x=0;
+	y=0;
+}
+Point::Point(double x,double y)
+{
+	this->x=x;
+	this->y=y;
+}
+double Point::getX()
+{
+	return x;
+}
+double Point::getY()
+{
+	return y;
+}	
+void Point::setX(double x)
+{
+	this->x=x;
+}
+void Point::setY(double y)
+{
+	this->y=y;
+} 
+void Point::printPoint()
+{
+	std::cout<<this->x<<" "<<this->y<<"\n";
+}
